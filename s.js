1edaadd5d05b479cbbac78f8db7a1a1b88caf687
Phani54321 <92142841+Phani54321@users.noreply.github.com>
2021-12-14 19:01:06 +0530

    m

---
+++ s.js
@@ -0,0 +1,31 @@
+var mystr="hello \"world\""
+console.log(mystr);
+
+var mystr="java script is a frontend language";
+console.log(mystr.length);
+
+var mystr="java script is a frontend language";
+console.log(mystr.indexOf('is'));
+
+var mystr="java script is a frontend language";
+console.log(mystr.lastIndexOf('is'));
+
+var str="java script is a frontend language";
+console.log(str.slice(0,7));
+console.log(str.slice(-10));
+
+var str="java script is a frontend language";
+console.log(str.toUpperCase("str"));
+
+var str="JAVA SCRIPT IS A FRONTEND LANGUAGE";
+console.log(str.toLowerCase(str));
+
+var fst="hi";
+var sec="hello";
+console.log(fst.concat('',sec));
+
+var str="h        elo            ";
+console.log(str.trim());
+
+var str='java script is a frontend language'
+console.log(str.charAt(5));
